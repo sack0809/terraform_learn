eec896fb21f10b2897404fc51b3f12fc8288d558
Saquib <er.saquib08@gmail.com>
2024-03-15 17:06:07 +0000

    Adding test user file in the git repo

---
+++ user.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "eu-west-1"  # Replace with your desired region
+}
+
+resource "aws_iam_user" "example" {
+  name = "myTest-user"
+}
+
+resource "aws_vpc" "myFirst" {
+    cid
+  
+}
